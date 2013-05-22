35d4f6c65ca7dffead6925afe5979dc06a0f4554
Adam Nicholls <inkysplat@gmail.com>
2013-05-22 14:52:56 +0100

    debug added

--- system/controllers/home.php
+++ system/controllers/home.php
@@ -3,7 +3,8 @@
 $args = array('lastfm', 'twitter');
 
 include_once('api.php');
-
 $twitter = json_decode($calls['twitter'],true);
-
 $lastfm = json_decode($calls['lastfm'], true);
+
+
+echo "<!-- ".print_r($calls,true)."-->";
--- system/views/pages/home.php
+++ system/views/pages/home.php
@@ -1,6 +1,7 @@
         <div class="container">
             <h1>Goram + Vincent Webcam</h1>
-            <img src="/webcam.jpg" alt="Webcam Image" width="640" height="480">
+            <img src="/webcam.jpg" alt="Webcam Image" width="640" height="480" title="<?= date('Y-m-d H:i:s',filemtime('webcam.jpg'))?>">
+	    
             <blockquote><?= $twitter[0]['text'];?> [<a href="http://www.twitter.com/goramandvincent" title="Twitter">twitter</a>]</blockquote>
             <blockquote><?= $lastfm['recenttracks']['track'][0]['artist']['#text'].' - '.$lastfm['recenttracks']['track'][0]['name']?> [<a href="http://www.last.fm/user/goramandvincent/" title="last.fm">lastfm</a>]</blockquote>
             <a href="/index.php?page=slideshow">View Timelapse</a>
