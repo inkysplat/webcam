507bb4524662bf612b84a971f25c7b957008abbe
Adam Nicholls <inkysplat@gmail.com>
2013-05-31 00:47:11 +0100

    tweaks

--- system/application/controllers/api.php
+++ system/application/controllers/api.php
@@ -9,30 +9,21 @@ Class ApiController extends Controller
 
 	public function githubServiceHookAction()
 	{
-
-		file_put_contents('/tmp/github_raw',print_r(array('get'=>$_GET,'post'=>$_POST,'files'=>$_FILES,'server'=>$_SERVER),true));
-
 		$request = Util('Request');
 
-		if($request->post('payload') || isset($request->params['payload']))
+		if($request->post('payload'))
 		{
-			if($request->post('payload') && $request->post('payload') != '')
-			{
-				$payload = $request->post('payload');
-			}
+			$payload = $request->post('payload');
 
-			if(isset($request->params['payload']) || $request->params['payload'] != '')
-			{
-				$payload = $request->params['payload'];
-			}
-
-			$cache = Util('cache');
+			$cache = Util('Cache');
+			
+			$cache->writeRaw('github.json',$payload);
 
 			$cache->setCacheFilename('github.json');
 			$cache->setCache('github',json_decode($payload));
 			$cache->writeCache();
 
-			$cache->writeRaw('github.json',$payload);
+			
 		}
 	}
 }
