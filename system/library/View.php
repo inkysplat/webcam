28569dfb7e6f33d7a879c865122475c25539aa0c
Adam Nicholls <inkysplat@gmail.com>
2013-05-30 10:52:39 +0100

    addedin XML rendering

--- system/library/View.php
+++ system/library/View.php
@@ -37,6 +37,9 @@ Class View
 			case 'json':
 				header('Content-type: application/json;');
 				break;
+			case 'xml':
+				header('Content-type: text/xml;');
+				break;
 			case 'html':
 				$this->header();
 				$this->footer();
@@ -53,6 +56,39 @@ Class View
 		}
 		return ob_get_clean();
 
+	}
+
+	public static function renderXML($data_array = array())
+	{
+		// creating object of SimpleXMLElement
+		$xml = new SimpleXMLElement("<?xml version=\"1.0\"?><root></root>");
+
+		// function call to convert array to xml
+		array_to_xml($data_array,$xml);
+
+		//saving generated xml file
+		$xml->asXML('file path and name');
+
+
+		// function defination to convert array to xml
+		function array_to_xml($data_array, &$xml) {
+		    foreach($data_array as $key => $value) {
+		        if(is_array($value)) {
+		            if(!is_numeric($key)){
+		                $subnode = $xml->addChild("$key");
+		                array_to_xml($value, $subnode);
+		            }
+		            else{
+		                array_to_xml($value, $xml);
+		            }
+		        }
+		        else {
+		            $xml->addChild("$key","$value");
+		        }
+		    }
+		}
+
+		return $xml->asXML();
 	}
 
 	public function partial($path, $params = array())
