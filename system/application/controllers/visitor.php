d918fd10106ba675892062d5bbbeca654f7c4b55
Adam Nicholls <inkysplat@gmail.com>
2013-06-06 12:02:41 +0100

    added comet polling chat

--- system/application/controllers/visitor.php
+++ system/application/controllers/visitor.php
@@ -4,9 +4,13 @@ Class VisitorController extends Controller
 {
 	public $defaultViewType = 'html';
 
+	private $messageFile = 'message.txt';
+
 	public function __construct($deps)
 	{
 		parent::__construct($deps);
+
+		$this->messageFile = CACHE_PATH.$this->messageFile;
 	}
 
 	public function addAction()
@@ -24,6 +28,52 @@ Class VisitorController extends Controller
 		$this->defaultViewType = 'plain';
 		$rs = $this->_model->latestVisitorCount($interval);
 		$this->viewParams['current'] = $rs[0]['count'];
+	}
+
+	public function getMessageAction()
+	{		  
+		  $this->defaultViewType = 'json';
+
+		  if(!file_exists($this->messageFile))
+		  {
+		  	return false;
+		  }
+
+		  $request = Util('Request');
+
+		  // infinite loop until the data file is not modified
+		  $lastmodif    = isset($request->params['timestamp']) ? $request->params['timestamp'] : 0;
+		  $currentmodif = filemtime($this->messageFile);
+		  while ($currentmodif <= $lastmodif) // check if the data file has been modified
+		  {
+		    usleep(10000); // sleep 10ms to unload the CPU
+		    clearstatcache();
+		    $currentmodif = filemtime($this->messageFile);
+		  }
+		 
+		  // return a json array
+		  $response = array();
+		  $response['msg']       = file_get_contents($this->messageFile);
+		  $response['timestamp'] = $currentmodif;
+
+		  $this->viewParams['response'] = $response;
+	}
+
+	public function postMessageAction($message = '')
+	{
+		$this->defaultViewType = 'html';
+
+		$request = Util('Request');
+		if($request->params['message'] != '')
+		{
+			if(!file_exists($this->messageFile))
+			{
+				touch($this->messageFile);
+				chmod($this->messageFile, 0777);
+			}
+
+			file_put_contents($this->messageFile, $request->params['message']);
+		}		
 	}
 
 }
